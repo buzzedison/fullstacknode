1e70fc25eb7d856d9f0a8ab7a4cc748efa3c9854
Edison <buzzedison@gmail.com>
2019-07-10 16:58:01 +0000

    edited home

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,7 @@
 // keys.js figure out what sets of credentials to return
 //production or development?
 
-if (process.env.Node_ENV === 'production'){
+if (process.env.NODE_ENV === 'production'){
     // we are in production. return the prod set of keys
     module.exports = require('./prod')
 }
